16f6057d0736ea7dfd6276417870b47c6f03d46c
Ravindra T <rtholiya@microsoft.com>
2023-03-23 16:31:28 +0000

    Fixed db update issue

--- db_connector.py
+++ db_connector.py
@@ -1,4 +1,5 @@
 import pymongo
+from bson.objectid import ObjectId
 
 class DBConnector:
     def __init__(self):
@@ -6,9 +7,9 @@ class DBConnector:
         client = pymongo.MongoClient(connection_string)
         self.db = client['mwizz_db']
 
-    def db_update_status(self,collection,request_id,state,url =None):
+    def db_update_status(self,collection,request_id,state,url="http://10.171.247.124:5601/app/dashboards#/view/5f9519f0-c97f-11ed-9b11-6945b3ed2302?_g=(refreshInterval:(pause:!t,value:0),time:(from:now-15m,to:now))&_a=(viewMode:view)"):
         if state =="done":
-            self.db[collection].update_one({ "_id" : request_id },{ "$set": {"state":state,"dashboardUrl": "https://localhost:3000"}},upsert=True)
+            self.db[collection].update_one({ "_id" : ObjectId(request_id) },{ "$set": {"state":state,"dashboardUrl": url}},upsert=True)
         else:
-            self.db[collection].update_one({ "_id" : request_id },{ "$set": {"state":state}},upsert=True)
-        
+            self.db[collection].update_one({ "_id" : (request_id) },{ "$set": {"state":state, "dashboardUrl": url}},upsert=True)
+
